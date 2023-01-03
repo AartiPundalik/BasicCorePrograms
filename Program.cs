ceeb5e154ad80624670bc965018dbfe00296d983
AartiPundalik <aartipundalik68@gmail.com>
2023-01-03 17:55:21 +0530

    program

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using yearcheck;
+using basiccoreprograms;
+using largtestnumber;
+using thepoweroftwo;
+using thefactors;
+using theevenodd;
+using thealphabet;
+using remainder;
+using swapnumber;
+using flipthecoin;
+using theharmonic;
+
+
+
+namespace basiccoreprograms
+{
+    public class program
+    {
+        public static void Main(String[] args)
+        {
+            flipcoin.FlipTheCoin();
+            leapyear.checktheleapyear();
+            powerof2.checkpowerof2();
+            harmonic.checkharmonic();
+            factors.checkfactors();
+            QR.checkQR();
+            swap.checkswap();
+            evenorodd.checkevenorodd();
+            alphabet.checkVowelsOrConstant();
+            Largest.checkLargestNumber();
+
+
+
+
+        }
+    }
+}
