026b5b96c0f0511d1d13c1e87af9079ddda41f38
AartiPundalik <aartipundalik68@gmail.com>
2023-01-03 17:51:12 +0530

    checkevenorodd

---
+++ evenorodd.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace theevenodd
+{
+    public class evenorodd
+    {
+        public static void checkevenorodd()
+        {
+            int Number;
+            Console.Write("Enter The Number: ");
+            Number = int.Parse(Console.ReadLine());
+            if (Number % 2 == 0)
+            {
+                Console.Write("Entered Number is an Even Number");
+            }
+            else
+            {
+                Console.Write("Entered Number is an Odd Number");
+            }
+            Console.Read();
+        }
+    }
+}
