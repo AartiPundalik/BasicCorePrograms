dd4b3d7b031748074a32b65c6fc99d37a59c5255
AartiPundalik <aartipundalik68@gmail.com>
2023-01-03 17:35:38 +0530

    flipcoin

---
+++ flipcoin.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace flipthecoin
+{
+    public class flipcoin
+    {
+        public static void FlipTheCoin()
+        {
+            int tailcount = 0;
+            int headcount = 0;
+            Console.WriteLine("please enter the value for number of flips");
+            int flip = Convert.ToInt32(Console.ReadLine());
+            if (flip > 0)
+            {
+                for (int i = 0; i < flip; i++)
+                {
+                    Random random = new Random();
+                    int number = random.Next();
+                    Console.WriteLine(number);
+
+                    if (number == 0)
+                    {
+                        tailcount++;
+                    }
+                    else
+                    {
+                        headcount++;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("please enter corrrect value");
+
+
+
+            }
+            Console.WriteLine("Headcount is" + headcount);
+            Console.WriteLine("tailcount is" + tailcount);
+
+            int headpercentage = headcount * 100 / flip;
+
+            int tailpercentage = tailcount * 100 / flip;
+
+            Console.WriteLine("head percentage is" + headpercentage);
+            Console.WriteLine("tail percentage is" + tailpercentage);
+
+
+
+
+        }
+
+    }
+    
+
+    }
+
